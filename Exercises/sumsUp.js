85a1ff0557243b05559c9a295482bb4efa942d36
Jonatan <jonatan.kruszewski@itc.tech>
2020-11-29 15:10:38 +0200

    Updated SumsUP

--- Exercises/sumsUp.js
+++ Exercises/sumsUp.js
@@ -1,22 +1,31 @@
 /*
-Create a function that gets every pair of numbers from an array that sums up to eight and returns it as an array of pairs (pair sorted ascendingly) collated into an object. See the following examples for more details.
+Create a function that gets every pair of numbers from an array that sums 
+up to eight and returns it as an array of pairs (pair sorted ascendingly) 
+collated into an object. See the following examples for more details.
 
 Examples
 sumsUp([1, 2, 3, 4, 5]) ➞ {"pairs": [[3, 5]]}
 
 sumsUp([10, 9, 7, 2, 8]) ➞ {"pairs": []}
 
-sumsUp([1, 6, 5, 4, 8, 2, 3, 7]) ➞ {"pairs": [[2, 6], [3, 5], [1, 7]]}
-// [6, 2] first to complete the cycle (to sum up to 8)
+The pairs should be ordered "First pair found, first placed".
+
+sumsUp([1, 6, 5, 4, 8, 2, 3, 7]) ➞ {"pairs": [[1, 7], [2, 6], [3, 5]]}
+// [1, 7] first to complete the cycle (to sum up to 8)
+// [6, 2] Second pair
 // [5, 3] follows
-// [1, 7] lastly
-// [2, 6], [3, 5], [1, 7] sorted according to cycle completeness, then pair-wise
+
+Don't forget to sort them before placing them in the answer:
+[6, 2] => Should be [2,6]
+[5, 3] => Should be [3,5]
+
+Solution: {"pairs": [[1, 7], [2, 6], [3, 5]]}
 Notes
 Remember the idea of "completes the cycle first" when getting the sort order of pairs.
 Only unique numbers are present in the array.
 */
 
-function sumsUp( /*args*/ ) {
+function sumsUp(/*args*/) {
   //your code
 }
 
